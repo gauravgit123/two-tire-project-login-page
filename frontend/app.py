7196c083fba2bc6f693bee38a1f4687648174340
gauravgit123 <135406634+gauravgit123@users.noreply.github.com>
2025-03-29 13:04:47 +0530

    Update app.py

--- frontend/app.py
+++ frontend/app.py
@@ -109,4 +109,4 @@ def success():
     return render_template('success.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True)
